67d5282b973ac465d16268166a1618bf2a06d4e0
ThanHtuAung <thanhtooaung.lucifer@gmail.com>
2023-02-22 15:13:24 +0630

    remove print

--- lib/src/tick_mark_shapes/text_shape.dart
+++ lib/src/tick_mark_shapes/text_shape.dart
@@ -9,34 +9,49 @@ class TextShape extends SliderTickMarkShape {
     required this.textList,
     this.textColor,
   });
+
   int i = 0;
 
   @override
-  Size getPreferredSize({required SliderThemeData sliderTheme, required bool isEnabled}) {
+  Size getPreferredSize(
+      {required SliderThemeData sliderTheme, required bool isEnabled}) {
     return Size.square(sliderTheme.trackHeight! * 1.5);
   }
 
   @override
-  void paint(PaintingContext context, Offset center, {required RenderBox parentBox, required SliderThemeData sliderTheme, required Animation<double> enableAnimation, required Offset thumbCenter, required bool isEnabled, required TextDirection textDirection}) {
+  void paint(PaintingContext context, Offset center,
+      {required RenderBox parentBox,
+      required SliderThemeData sliderTheme,
+      required Animation<double> enableAnimation,
+      required Offset thumbCenter,
+      required bool isEnabled,
+      required TextDirection textDirection}) {
     Color? begin;
     Color? end;
 
-    print(i);
     switch (textDirection) {
       case TextDirection.ltr:
         final bool isTickMarkRightOfThumb = center.dx > thumbCenter.dx;
-        begin = isTickMarkRightOfThumb ? sliderTheme.disabledInactiveTickMarkColor : sliderTheme.disabledActiveTickMarkColor;
-        end = isTickMarkRightOfThumb ? sliderTheme.inactiveTickMarkColor : sliderTheme.activeTickMarkColor;
+        begin = isTickMarkRightOfThumb
+            ? sliderTheme.disabledInactiveTickMarkColor
+            : sliderTheme.disabledActiveTickMarkColor;
+        end = isTickMarkRightOfThumb
+            ? sliderTheme.inactiveTickMarkColor
+            : sliderTheme.activeTickMarkColor;
         break;
       case TextDirection.rtl:
         final bool isTickMarkLeftOfThumb = center.dx < thumbCenter.dx;
-        begin = isTickMarkLeftOfThumb ? sliderTheme.disabledInactiveTickMarkColor : sliderTheme.disabledActiveTickMarkColor;
-        end = isTickMarkLeftOfThumb ? sliderTheme.inactiveTickMarkColor : sliderTheme.activeTickMarkColor;
+        begin = isTickMarkLeftOfThumb
+            ? sliderTheme.disabledInactiveTickMarkColor
+            : sliderTheme.disabledActiveTickMarkColor;
+        end = isTickMarkLeftOfThumb
+            ? sliderTheme.inactiveTickMarkColor
+            : sliderTheme.activeTickMarkColor;
         break;
     }
-    final Paint paint = Paint()..color = ColorTween(begin: begin, end: end).evaluate(enableAnimation)!;
+    final Paint paint = Paint()
+      ..color = ColorTween(begin: begin, end: end).evaluate(enableAnimation)!;
     paint.style = PaintingStyle.fill;
-
 
     final textStyle = TextStyle(
       color: textColor,
@@ -51,7 +66,6 @@ class TextShape extends SliderTickMarkShape {
       maxLines: 2,
       text: textSpan,
       textDirection: TextDirection.ltr,
-
     );
     textPainter.layout(
       minWidth: 0,
@@ -60,20 +74,21 @@ class TextShape extends SliderTickMarkShape {
 
     // The tick marks are tiny circles that are the same height as the track.
     final double tickMarkRadius = getPreferredSize(
-      isEnabled: isEnabled,
-      sliderTheme: sliderTheme,
-    ).width / 2;
+          isEnabled: isEnabled,
+          sliderTheme: sliderTheme,
+        ).width /
+        2;
 
     if (tickMarkRadius > 0) {
-      final textCenter = Offset(center.dx - (textPainter.width / 2), center.dy - (textPainter.height /2));
+      final textCenter = Offset(center.dx - (textPainter.width / 2),
+          center.dy - (textPainter.height / 2));
 
       textPainter.paint(context.canvas, textCenter);
       // context.canvas.drawRect(Offset((center.dx - (tickMarkRadius) / 2), (center.dy - (tickMarkRadius * 3) / 2)) & Size(tickMarkRadius, tickMarkRadius * 3), paint);
     }
     i++;
-    if(i == textList.length) {
+    if (i == textList.length) {
       i = 0;
     }
   }
-
 }
